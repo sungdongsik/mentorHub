7f54711c8bf60a1af362445af1f27d368c92e43d
성동식 <tjdehdtlr123@naver.com>
2025-11-26 22:54:27 +0900

    통합 테스트 - 조회, 조회 키워드, 등록 테스트 추가

--- module-core/src/test/java/com/mentorHub/api/controller/MenteeControllerTest.java
+++ module-core/src/test/java/com/mentorHub/api/controller/MenteeControllerTest.java
@@ -1,48 +1,102 @@
 package com.mentorHub.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mentorHub.api.dto.PageResponse;
+import com.mentorHub.api.dto.request.CreateMenteeRequest;
+import com.mentorHub.api.dto.response.MenteeResponse;
+import com.util.ApiResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.boot.test.context.SpringBootTest.*;
 
 
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class MenteeControllerTest {
 
+    @LocalServerPort
+    private int port;
+
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
 
     @Test
-    @DisplayName("멘티 리스트 목록")
-    void testGetMentors() throws Exception {
+    @DisplayName("멘티 리스트 목록 키워드 없이 조회하기")
+    void testGetMenteesNoKeyword() throws Exception {
 
-        mockMvc.perform(get("/api/mentors"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(200))
-                .andExpect(jsonPath("$.message").value("SUCCESS"))
-                .andExpect(jsonPath("$.data.contents").isArray())
-                .andExpect(jsonPath("$.data.totalCount").value(20))
-                .andExpect(jsonPath("$.data.totalPages").value(1))
-                .andExpect(jsonPath("$.data.pageSize").value(20))
-                .andDo(print());
+        String url = "http://localhost:" + port + "/api/mentees";
+
+        ResponseEntity<ApiResponse<PageResponse<MenteeResponse>>> response =
+                restTemplate.exchange(
+                        url,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<ApiResponse<PageResponse<MenteeResponse>>>() {}
+                );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getData().getContents().get(0).getName()).isEqualTo("Mentee1");
+
+        response.getBody().getData().getContents()
+                .forEach(m -> System.out.println(m.getName()));
+    }
+
+    @Test
+    @DisplayName("멘티 리스트 목록 키워드 포함 조회하기")
+    void testGetMenteesYesKeyword() throws Exception {
+
+        String url = "http://localhost:" + port + "/api/mentees?name=Mentee1&status=MENTEE&keyword=Java";
+
+        ResponseEntity<ApiResponse<PageResponse<MenteeResponse>>> response =
+                restTemplate.exchange(
+                        url,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<ApiResponse<PageResponse<MenteeResponse>>>() {}
+                );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getData().getContents().get(0).getName()).isEqualTo("Mentee10");
+
+        response.getBody().getData().getContents()
+                .forEach(m -> System.out.println(m.getName()));
     }
 
     @Test
     @DisplayName("멘티 글 등록")
     void testSetMentors() throws Exception {
+        // 요청 객체 생성
+        CreateMenteeRequest request = new CreateMenteeRequest();
+        request.setTitle("Test Title");
+        request.setContent("Test Content");
+        request.setStartDate(LocalDateTime.now());
+        request.setKeyword(new String[]{"Java", "jsp"});
+        request.setJob("Test Job");
 
-        mockMvc.perform(post("/api/mentors"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(200))
-                .andExpect(jsonPath("$.message").value("SUCCESS"))
-                .andDo(print());
+        String url = "http://localhost:" + port + "/api/mentees";
+
+        // POST 요청
+        ResponseEntity<ApiResponse> response = restTemplate.postForEntity(url, request, ApiResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(200, response.getBody().getStatus());
+
+        System.out.println(
+                new ObjectMapper().writerWithDefaultPrettyPrinter()
+                        .writeValueAsString(response.getBody())
+        );
     }
 }
