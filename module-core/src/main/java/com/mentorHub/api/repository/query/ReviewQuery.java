c1f61df36b6112bad39299c0804bd780cc13e964
성동식 <tjdehdtlr123@naver.com>
2025-12-21 16:09:24 +0900

    댓글 api 로직 수정 및 LocalDateTime 응답 포맷 설정 클래스 추가

--- module-core/src/main/java/com/mentorHub/api/repository/query/ReviewQuery.java
+++ module-core/src/main/java/com/mentorHub/api/repository/query/ReviewQuery.java
@@ -1,13 +1,14 @@
 package com.mentorHub.api.repository.query;
 
-import com.mentorHub.api.entity.QReviewEntity;
 import com.mentorHub.api.entity.ReviewEntity;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.mentorHub.api.entity.QMenteeEntity.menteeEntity;
 import static com.mentorHub.api.entity.QReviewEntity.reviewEntity;
 
 @Repository
@@ -20,6 +21,18 @@ public class ReviewQuery {
         return queryFactory.select(reviewEntity)
                 .from(reviewEntity)
                 .where(reviewEntity.mentee.writingId.in(writingIds))
+                .fetch();
+    }
+
+
+    public List<ReviewEntity> getReview() {
+        return queryFactory
+                .selectFrom(reviewEntity)
+                .join(reviewEntity.mentee, menteeEntity).fetchJoin()
+                .where(
+                        menteeEntity.delYn.eq("N"),
+                        reviewEntity.delYn.eq("N")
+                )
                 .fetch();
     }
 }
