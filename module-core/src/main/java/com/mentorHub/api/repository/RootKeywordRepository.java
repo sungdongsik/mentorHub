cdb558f5c668a103224c00fcb863562ab268827f
성동식 <tjdehdtlr123@naver.com>
2026-02-14 18:23:45 +0900

    관리자에서 키워드 승인 로직 수정중 2차

--- module-core/src/main/java/com/mentorHub/api/repository/RootKeywordRepository.java
+++ module-core/src/main/java/com/mentorHub/api/repository/RootKeywordRepository.java
@@ -11,18 +11,17 @@ import java.util.List;
 
 public interface RootKeywordRepository extends JpaRepository<RootKeywordEntity, Long> {
 
-    @Query("""
-    SELECT DISTINCT rk
-    FROM RootKeywordEntity rk
-    JOIN RootKeywordAliasEntity rka
-      ON rka.rootKeyword = rk
-    WHERE rka.aliasName IN :aliasName
-    """)
-    List<RootKeywordEntity> findByCanonicalName(@Param("aliasName") List<String> aliasNames);
-
     List<RootKeywordEntity> findAllByStatus(RootKeywordAliasStatus status);
 
     @Modifying(clearAutomatically = true)
     @Query("UPDATE RootKeywordEntity rk SET rk.status = :newStatus WHERE rk.status = :oldStatus")
     int bulkUpdateStatus(@Param("oldStatus") RootKeywordAliasStatus oldStatus, @Param("newStatus") RootKeywordAliasStatus newStatus);
+
+    @Query("""
+    SELECT DISTINCT rk
+    FROM RootKeywordEntity rk
+    JOIN RootKeywordAliasEntity rka ON rka.rootKeyword = rk
+    WHERE rka.aliasName IN :aliasNames
+    """)
+    List<RootKeywordEntity> findCanonicalName(@Param("aliasNames") List<String> aliasNames);
 }
