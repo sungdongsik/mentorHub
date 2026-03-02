9e67f092a9b7156efe94e49cf5dcf0c4213cb5e7
성동식 <tjdehdtlr123@naver.com>
2026-03-02 21:38:43 +0900

    이메일 보낼 시 로그 entity 추가

--- module-core/src/main/java/com/mentorHub/api/scheduler/RootKeywordScheduler.java
+++ module-core/src/main/java/com/mentorHub/api/scheduler/RootKeywordScheduler.java
@@ -1,6 +1,7 @@
 package com.mentorHub.api.scheduler;
 
 import com.mentorHub.api.entity.RootKeywordAliasEntity;
+import com.mentorHub.api.service.EmailHistoryService;
 import com.mentorHub.api.service.EmailService;
 import com.mentorHub.api.service.RootKeywordService;
 import com.util.RootKeywordAliasStatus;
@@ -21,25 +22,29 @@ public class RootKeywordScheduler {
 
     private final RootKeywordService rootKeywordService;
     private final EmailService emailService;
+    private final EmailHistoryService emailHistoryService;
 
     @Value("${spring.mail.username}")
     private String adminEmail;
 
     // 매일 새벽 3시에 실행 (초 분 시 일 월 요일)
     @Scheduled(cron = "0 0 3 * * *")
-    @Transactional(readOnly = true)
+    @Transactional
     public void sendPendingKeywordsByEmail() {
         log.info("승인 대기 중인 키워드 이메일 발송 스케줄러 시작...");
 
         List<RootKeywordAliasEntity> pendingAliases = rootKeywordService.getKeywordApproval(RootKeywordAliasStatus.PENDING);
 
         if (!pendingAliases.isEmpty()) {
+            String subject = "승인 대기 중인 키워드 목록";
             String emailContent = pendingAliases.stream()
                     .map(alias -> "키워드명: " + alias.getAliasName() + ", 상태: " + alias.getStatus().getName())
                     .collect(Collectors.joining("\n"));
 
-            emailService.sendSimpleMessage(adminEmail, "승인 대기 중인 키워드 목록", emailContent);
-            log.info("이메일 발송 완료. 총 {}개의 대기 중인 키워드가 전송되었습니다.", pendingAliases.size());
+            emailService.sendSimpleMessage(adminEmail, subject, emailContent);
+            emailHistoryService.saveEmailHistory(adminEmail, subject, emailContent);
+
+            log.info("이메일 발송 및 히스토리 저장 완료. 총 {}개의 대기 중인 키워드가 전송되었습니다.", pendingAliases.size());
         } else {
             log.info("전송할 대기 중인 키워드가 없습니다.");
         }
